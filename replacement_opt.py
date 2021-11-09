c89007c6c7c3c3c2e2ca3c71347ebaec2936c15b
bedabrata <bedabratap909@gmail.com>
2021-11-09 18:23:45 +0530

    REPLACEMENT OPERATOR

---
+++ replacement_opt.py
@@ -0,0 +1,4 @@
+name=input("Enter Name:")
+salary=eval(input("Enter your Salary:"))
+friend=input("Enter your friend's name:")
+print('Hello {}, your salary is {} and your friend {} is waiting'.format(name,salary,friend))
